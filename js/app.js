e8cf446bcc129a909f1ac0b5f91efc4c0dafa35d
marcelonader <marcelo.m.nader@gmail.com>
2021-09-22 23:58:55 -0300

    cria funções de adicionar e remover tarefa

--- js/app.js
+++ js/app.js
@@ -5,18 +5,67 @@
   <p class="timestamp">Criada: 19/04/20</p>
 </div>
 </li> */
-const ulTarefasPendentes = document.querySelector('.tarefas-pendentes');
-const ulTarefasConcluidas = document.querySelector('ul[class=titulo-concluida]');
-console.log(ulTarefasConcluidas)
+const $ulTarefasPendentes = document.querySelector('.tarefas-pendentes');
+const $ulTarefasConcluidas = document.querySelector('ul[class=titulo-concluida]');
+const $inputNovaTarefa = document.querySelector('#input-nova-tarefa');
+const $btnAddTarefa = document.querySelector('.btn-add-tarefa');
 
+
+let countId = 0;
+/**
+ * Adiciona uma nova tarefa na "ul" de tarefas pendentes do HTML. No final, acrescenta mais 1 à variável "countId", para que o id de cada tarefa seja dinâmico.
+ */
+const addTarefa = ()=>{
+    $ulTarefasPendentes.insertAdjacentHTML('afterbegin', 
+    `
+    <li class="tarefa" id='li${countId}'>
+        <input id='${countId}' type='checkbox' class='checkbox'>
+        <label for='${countId}' class="not-done"></label>
+        <div class="descripcion">
+
+                <p class="nome">${$inputNovaTarefa.value}</p>
+            <div>
+                <p class="timestamp">Criada: ${new Date().toLocaleDateString('pt-BR')}</p>
+
+                <button onclick='deletarTarefa(li${countId})' class='btn-deletar-tarefa'><img src='../assets/highlight_off_black_24dp.svg' alt=''></button>
+            </div>
+        </div>
+    </li>
+    `)
+    countId++;
+}
+
+//Esses arrays armazenarão os objetos "tarefas" que vierem do endpoint "https://jsonplaceholder.typicode.com/todos/". Cada objeto conterá um "id" e um "title".
 let listaTarefasPendentes=[];
 let listaTarefasConcluidas=[];
 
 
+$btnAddTarefa.addEventListener('click', e=>{
+    addTarefa();
+    e.preventDefault();
+})
+
+
+/**
+ * Deleta a tarefa da aplicação.
+ * @param {*} id 
+ */
+const deletarTarefa=id=>{
+    const el = document.querySelector(`#${id.id}`);
+    el.parentNode.removeChild(el);
+}
+
+
+
+/**
+ * Recupera e trata os dados da Fake API "jsonplaceholder".
+ */
 (async()=>{
     let response = await fetch('https://jsonplaceholder.typicode.com/todos/');
     let jsonData = await response.json()
+
     jsonData.forEach((el, i)=>{
+        
         if(el.completed){
             listaTarefasConcluidas.push({
                 id: el.id,
@@ -30,4 +79,3 @@ let listaTarefasConcluidas=[];
         }
     })
 })()
-
