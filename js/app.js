d8997e0ca0b5f76ed87cd34071b322beb67e943d
marcelonader <marcelo.m.nader@gmail.com>
2021-09-23 16:38:48 -0300

    atualização no projeto

--- js/app.js
+++ js/app.js
@@ -5,33 +5,73 @@
   <p class="timestamp">Criada: 19/04/20</p>
 </div>
 </li> */
+
+// let DateFormat = moment().format('DD/MM/YYYY');
+
 const $ulTarefasPendentes = document.querySelector('.tarefas-pendentes');
 const $ulTarefasConcluidas = document.querySelector('ul[class=titulo-concluida]');
 const $inputNovaTarefa = document.querySelector('#input-nova-tarefa');
 const $btnAddTarefa = document.querySelector('.btn-add-tarefa');
+const today = new Date().toLocaleDateString('pt-BR')
+const $dataConclusao = document.querySelector('.data-conclusao')
 
+
+
+// const dataMoment = moment(data, 'DD/MM/YYYY')
+// const calendario = document.querySelector('.data-conclusao'); 
+// const data = moment(calendario.value)
+// let dateFormat = moment(calendario.value).format('DD/MM/YYYY')
+// const dataMoment = moment(data, 'DD/MM/YYYY')
+let data = new Date().toLocaleDateString('en').split('/');
+let dataTratada;
+if(data[0]<10 && data[1]<10){
+   dataTratada=`${data[2]}-0${data[0]}-0${data[1]}`
+}else if(data[0]<10 && data[1]>10){
+    dataTratada=`${data[2]}-0${data[0]}-${data[1]}`
+}else if(data[0]>10 && data[1]<10){
+    dataTratada=`${data[2]}-${data[0]}-0${data[1]}`
+}else{
+    dataTratada=`${data[2]}-${data[0]}-${data[1]}`
+}
+
+$dataConclusao.min=dataTratada
 
 let countId = 0;
 /**
  * Adiciona uma nova tarefa na "ul" de tarefas pendentes do HTML. No final, acrescenta mais 1 à variável "countId", para que o id de cada tarefa seja dinâmico.
  */
+
+const abrirModal=id=>{
+    document.querySelector('#modal').style.cssText=
+    `
+        display: block;
+    `
+    document.querySelector('#btn-deletar').addEventListener('click', e=>{
+        deletarTarefa(id)
+        document.querySelector('#modal').style.cssText=
+        `
+            display: none;
+        `
+    })
+    
+}
 const addTarefa = ()=>{
-    $ulTarefasPendentes.insertAdjacentHTML('afterbegin', 
+    $ulTarefasPendentes.insertAdjacentHTML('afterbegin',
     `
     <li class="tarefa" id='li${countId}'>
         <input id='${countId}' type='checkbox' class='checkbox'>
         <label for='${countId}' class="not-done"></label>
         <div class="descripcion">
-
                 <p class="nome">${$inputNovaTarefa.value}</p>
             <div>
-                <p class="timestamp">Criada: ${new Date().toLocaleDateString('pt-BR')}</p>
-
-                <button onclick='deletarTarefa(li${countId})' class='btn-deletar-tarefa'><img src='../assets/highlight_off_black_24dp.svg' alt=''></button>
+                <p class="timestamp">Criada: ${today}</p>
+                <p class="timestamp">Conclusão: ${$dataConclusao.value}</p>
+                <button onclick='abrirModal(li${countId})' class='btn-deletar-tarefa'><img src='../assets/highlight_off_black_24dp.svg' alt=''></button>
             </div>
         </div>
     </li>
     `)
+    // <button onclick='deletarTarefa(li${countId})' class='btn-deletar-tarefa'><img src='../assets/highlight_off_black_24dp.svg' alt=''></button>
     countId++;
 }
 
