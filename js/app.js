7fb8e9b675af8941788815dd2996df9a3ccf1509
mariobiondog <biondo.dev@gmail.com>
2021-09-23 21:57:24 -0300

    validacao do input de task

--- js/app.js
+++ js/app.js
@@ -8,56 +8,61 @@
 
 // let DateFormat = moment().format('DD/MM/YYYY');
 
-const $ulTarefasPendentes = document.querySelector('.tarefas-pendentes');
-const $ulTarefasConcluidas = document.querySelector('ul[class=titulo-concluida]');
-const $inputNovaTarefa = document.querySelector('#input-nova-tarefa');
-const $btnAddTarefa = document.querySelector('.btn-add-tarefa');
-const today = new Date().toLocaleDateString('pt-BR')
-const $dataConclusao = document.querySelector('.data-conclusao')
-
-
+const $ulTarefasPendentes = document.querySelector(".tarefas-pendentes");
+const $ulTarefasConcluidas = document.querySelector(
+  "ul[class=titulo-concluida]"
+);
+const $inputNovaTarefa = document.querySelector("#input-nova-tarefa");
+const $btnAddTarefa = document.querySelector(".btn-add-tarefa");
+const today = new Date().toLocaleDateString("pt-BR");
+const $dataConclusao = document.querySelector(".data-conclusao");
 
 // const dataMoment = moment(data, 'DD/MM/YYYY')
-// const calendario = document.querySelector('.data-conclusao'); 
+// const calendario = document.querySelector('.data-conclusao');
 // const data = moment(calendario.value)
 // let dateFormat = moment(calendario.value).format('DD/MM/YYYY')
 // const dataMoment = moment(data, 'DD/MM/YYYY')
-let data = new Date().toLocaleDateString('en').split('/');
+let data = new Date().toLocaleDateString("en").split("/");
 let dataTratada;
-if(data[0]<10 && data[1]<10){
-   dataTratada=`${data[2]}-0${data[0]}-0${data[1]}`
-}else if(data[0]<10 && data[1]>10){
-    dataTratada=`${data[2]}-0${data[0]}-${data[1]}`
-}else if(data[0]>10 && data[1]<10){
-    dataTratada=`${data[2]}-${data[0]}-0${data[1]}`
-}else{
-    dataTratada=`${data[2]}-${data[0]}-${data[1]}`
+if (data[0] < 10 && data[1] < 10) {
+  dataTratada = `${data[2]}-0${data[0]}-0${data[1]}`;
+} else if (data[0] < 10 && data[1] > 10) {
+  dataTratada = `${data[2]}-0${data[0]}-${data[1]}`;
+} else if (data[0] > 10 && data[1] < 10) {
+  dataTratada = `${data[2]}-${data[0]}-0${data[1]}`;
+} else {
+  dataTratada = `${data[2]}-${data[0]}-${data[1]}`;
 }
 
-$dataConclusao.min=dataTratada
+$dataConclusao.min = dataTratada;
 
 let countId = 0;
 /**
  * Adiciona uma nova tarefa na "ul" de tarefas pendentes do HTML. No final, acrescenta mais 1 à variável "countId", para que o id de cada tarefa seja dinâmico.
  */
 
-const abrirModal=id=>{
-    document.querySelector('#modal').style.cssText=
-    `
+const abrirModal = (id) => {
+  document.querySelector("#modal").style.cssText = `
         display: block;
-    `
-    document.querySelector('#btn-deletar').addEventListener('click', e=>{
-        deletarTarefa(id)
-        document.querySelector('#modal').style.cssText=
-        `
+    `;
+  document.querySelector("#btn-deletar").addEventListener("click", (e) => {
+    deletarTarefa(id);
+    document.querySelector("#modal").style.cssText = `
             display: none;
-        `
-    })
-    
-}
-const addTarefa = ()=>{
-    $ulTarefasPendentes.insertAdjacentHTML('afterbegin',
-    `
+        `;
+  });
+};
+
+const addTarefa = () => {
+  if ($inputNovaTarefa.value.length < 10) {
+    document.querySelector(".nova-tarefa").style.cssText = `
+    border: 2px solid red;
+    `; // ! falta tirar o vermelho dps que add a task
+    $btnAddTarefa.setAttribute("disabled");
+  } else {
+    $ulTarefasPendentes.insertAdjacentHTML(
+      "afterbegin",
+      `
     <li class="tarefa" id='li${countId}'>
         <input id='${countId}' type='checkbox' class='checkbox'>
         <label for='${countId}' class="not-done"></label>
@@ -70,52 +75,49 @@ const addTarefa = ()=>{
             </div>
         </div>
     </li>
-    `)
+    `
+    );
     // <button onclick='deletarTarefa(li${countId})' class='btn-deletar-tarefa'><img src='../assets/highlight_off_black_24dp.svg' alt=''></button>
     countId++;
-}
+  }
+};
 
 //Esses arrays armazenarão os objetos "tarefas" que vierem do endpoint "https://jsonplaceholder.typicode.com/todos/". Cada objeto conterá um "id" e um "title".
-let listaTarefasPendentes=[];
-let listaTarefasConcluidas=[];
+let listaTarefasPendentes = [];
+let listaTarefasConcluidas = [];
 
-
-$btnAddTarefa.addEventListener('click', e=>{
-    addTarefa();
-    e.preventDefault();
-})
-
+$btnAddTarefa.addEventListener("click", (e) => {
+  addTarefa();
+  e.preventDefault();
+});
 
 /**
  * Deleta a tarefa da aplicação.
- * @param {*} id 
+ * @param {*} id
  */
-const deletarTarefa=id=>{
-    const el = document.querySelector(`#${id.id}`);
-    el.parentNode.removeChild(el);
-}
-
-
+const deletarTarefa = (id) => {
+  const el = document.querySelector(`#${id.id}`);
+  el.parentNode.removeChild(el);
+};
 
 /**
  * Recupera e trata os dados da Fake API "jsonplaceholder".
  */
-(async()=>{
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos/');
-    let jsonData = await response.json()
+(async () => {
+  let response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  let jsonData = await response.json();
 
-    jsonData.forEach((el, i)=>{
-        
-        if(el.completed){
-            listaTarefasConcluidas.push({
-                id: el.id,
-                title: el.title
-            })
-        } else {
-            listaTarefasPendentes.push({
-                id: el.id,
-                title: el.title
-            })
-        }
-    })
-})()
+  jsonData.forEach((el, i) => {
+    if (el.completed) {
+      listaTarefasConcluidas.push({
+        id: el.id,
+        title: el.title,
+      });
+    } else {
+      listaTarefasPendentes.push({
+        id: el.id,
+        title: el.title,
+      });
+    }
+  });
+})();
